60cae51275b1bc4135fe2d9743c5550bf6b37f0e
Nostromo <nostromo@MacBook-Pro-Nostromo.local>
2023-07-27 22:21:49 +0200

    Added example of expo-image not working correctly with .apng files on android

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image style={styles.image} source={require('./assets/apng-sample.png')}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 300,
+    height: 200,
+  }
 });
